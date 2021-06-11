0d0c305c16a28f6bd37d6b8982f2a8cfc83c7bdc
Juli <serresjuli@gmail.com>
2021-06-10 22:15:41 -0300

    desafio1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nonni - tejidos a mano para tu bebe
         </p>
         <a
           className="App-link"
