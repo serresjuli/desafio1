467dd75fb8828e1d405e0e8ce436eea52baabefd
Juli <serresjuli@gmail.com>
2021-06-28 22:41:04 -0300

    contador con boton

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,5 +1,14 @@
+import {ItemCount} from '../itemCount/itemCount'
+
 export const ItemListContainer =({greeting}) =>{
+    const stock = 15;
+    const initial = 1;
+    const onAdd = () =>{}
+
     return(
-    <p>{greeting}</p>
+        <section>
+            <p>{greeting}</p>
+            <ItemCount stock={15} initial={1} onAdd={onAdd}/>
+        </section>
     )
 }
---
+++ src/components/itemCount/itemCount.js
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+
+export const ItemCount = ({stock, initial, onAdd}) =>{
+    
+    const [contador, setContador] = useState(initial)
+    const [sumaStock, setSumaStock] = useState(stock)
+
+    const btnSuma = () => {
+        if (sumaStock> 1 && contador<sumaStock){
+            setContador ( contador + 1)
+        }
+    }
+
+    const btnResta = () => {
+        if (contador>1){
+            setContador (contador - 1)
+        }
+        else if (contador<= 1 ){
+            setContador (contador + 0)
+        }
+    }
+
+    return(
+        <div className='contadorBuy'>
+            <button onClick={btnResta}> - </button> 
+            <span>{contador}</span> 
+            <button onClick={btnSuma}> + </button>
+            <button className='btnSumarCarrito' onClick={onAdd} disabled={false}> Agregar al carrito </button>
+        </div>
+    )
+}
